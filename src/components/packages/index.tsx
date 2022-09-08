6f087d684d1adb0ab6bbc0e4588a8b11734e4fcf
stojanovic98m <stojanovic98m@gmail.com>
2022-09-08 14:02:27 +0200

    section now fully responsive

--- src/components/packages/index.tsx
+++ src/components/packages/index.tsx
@@ -18,69 +18,82 @@ export const Packages = (props: PackageInterface) => {
       {props?.pricesData.map((data) => {
         const { id, name, price, bandwidth, memory, support, update } = data;
         return (
-          <VStack key={id} w="100%">
-            <Center mt="2rem" h="70px" bgColor="orange.500">
+          <VStack
+            key={id}
+            w="100%"
+            flexDirection={["column", "column", "column", "row"]}
+            alignItems="center"
+            mb="1rem"
+          >
+            <Center
+              w={["100%", "100%", "100%", "200px"]}
+              mt={["2rem", "2rem", "2rem", "0rem"]}
+              h="70px"
+              bgColor="orange.500"
+            >
               <Heading size="md" letterSpacing="1px">
                 {name}
               </Heading>
             </Center>
             <Heading
+              bold
               textAlign="center"
-              w="100%"
-              p="15px"
+              size="md"
+              w={["100%", "100%", "100%", "50%"]}
+              p={["15px", "15px", "15px", "0px"]}
               fontWeight="400"
-              borderBottomWidth="1px"
-              borderBottomColor="gray.400"
-              color="orange.600"
+              borderBottomWidth={["1px", "1px", "1px", "0px"]}
+              borderBottomColor={["gray.400", "gray.400", "gray.400", "none"]}
+              color="orange.500"
             >
               {price}
             </Heading>
 
             <Text
               textAlign="center"
-              w="100%"
-              p="15px"
-              borderBottomWidth="1px"
-              borderBottomColor="gray.400"
-              color="gray.600"
+              w={["100%", "100%", "100%", "50%"]}
+              p={["15px", "15px", "15px", "0px"]}
+              borderBottomWidth={["1px", "1px", "1px", "0px"]}
+              borderBottomColor={["gray.400", "gray.400", "gray.400", "none"]}
+              color={["gray.600", "gray.600", "black", "black"]}
             >
               {bandwidth} bandwidth
             </Text>
             <Text
               textAlign="center"
-              w="100%"
-              p="15px"
-              borderBottomWidth="1px"
-              borderBottomColor="gray.400"
-              color="gray.600"
+              w={["100%", "100%", "100%", "50%"]}
+              p={["15px", "15px", "15px", "0px"]}
+              borderBottomWidth={["1px", "1px", "1px", "0px"]}
+              borderBottomColor={["gray.400", "gray.400", "gray.400", "none"]}
+              color={["gray.600", "gray.600", "black", "black"]}
             >
               {memory} memory
             </Text>
             <Text
               textAlign="center"
-              w="100%"
-              p="15px"
-              borderBottomWidth="1px"
-              borderBottomColor="gray.400"
-              color="gray.600"
+              w={["100%", "100%", "100%", "50%"]}
+              p={["15px", "15px", "15px", "0px"]}
+              borderBottomWidth={["1px", "1px", "1px", "0px"]}
+              borderBottomColor={["gray.400", "gray.400", "gray.400", "none"]}
+              color={["gray.600", "gray.600", "black", "black"]}
             >
               {support} support
             </Text>
             <Text
               textAlign="center"
-              w="100%"
-              p="15px"
-              borderBottomWidth="1px"
-              borderBottomColor="gray.400"
-              color="gray.600"
+              w={["100%", "100%", "100%", "50%"]}
+              p={["15px", "15px", "15px", "0px"]}
+              borderBottomWidth={["1px", "1px", "1px", "0px"]}
+              borderBottomColor={["gray.400", "gray.400", "gray.400", "none"]}
+              color={["gray.600", "gray.600", "black", "black"]}
             >
               {update} update
             </Text>
 
             <Button
-              mt="10px"
+              mt={["10px", "10px", "10px", "0px"]}
               h="50px"
-              w="100%"
+              w={["100%", "100%", "100%", "150px"]}
               bgColor="gray.800"
               variant="outline"
               _text={{
